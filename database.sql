ca868ca47188040fb0c9d38dc467111738f2c9e6
Brady Baker <bradybaker82@gmail.com>
2021-01-11 16:09:07 -0600

    Updated registration form, db name in pool

--- database.sql
+++ database.sql
@@ -5,6 +5,10 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
+    "f_name" VARCHAR (50) NOT NULL,
+    "l_name" VARCHAR (50) NOT NULL,
+    "email" VARCHAR (50) NOT NULL,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "searchable" BOOLEAN NOT NULL
 );
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 class RegisterForm extends Component {
   state = {
-    username: '',
-    password: '',
+    newUser: {
+      fName: '',
+      lName: '',
+      email: '',
+      username: '',
+      password: '',
+      searchable: ''
+    }
   };
 
   registerUser = (event) => {
     event.preventDefault();
-
-    this.props.dispatch({
-      type: 'REGISTER',
-      payload: {
-        username: this.state.username,
-        password: this.state.password,
-      },
-    });
+    this.props.dispatch({ type: 'REGISTER', payload: this.state.newUser })
   }; // end registerUser
 
   handleInputChangeFor = (propertyName) => (event) => {
     this.setState({
-      [propertyName]: event.target.value,
+      newUser: {
+        [propertyName]: event.target.value,
+      }
     });
   };
 
   render() {
+    const { fName, lName, email, username, password, searchable } = this.state.newUser
     return (
       <form className="formPanel" onSubmit={this.registerUser}>
         <h2>Register User</h2>
@@ -36,14 +43,50 @@ class RegisterForm extends Component {
           </h3>
         )}
         <div>
+          <label htmlFor="fName">
+            First Name:
+            <input
+              type="text"
+              name="fName"
+              value={fName}
+              required
+              onChange={(event) => this.handleInputChangeFor(event, 'fName')}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="lName">
+            Last Name:
+            <input
+              type="text"
+              name="username"
+              value={lName}
+              required
+              onChange={(event) => this.handleInputChangeFor(event, 'lName')}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            E-mail:
+            <input
+              type="text"
+              name="email"
+              value={email}
+              required
+              onChange={(event) => this.handleInputChangeFor(event, 'email')}
+            />
+          </label>
+        </div>
+        <div>
           <label htmlFor="username">
             Username:
             <input
               type="text"
               name="username"
-              value={this.state.username}
+              value={username}
               required
-              onChange={this.handleInputChangeFor('username')}
+              onChange={(event) => this.handleInputChangeFor(event, 'username')}
             />
           </label>
         </div>
@@ -53,11 +96,20 @@ class RegisterForm extends Component {
             <input
               type="password"
               name="password"
-              value={this.state.password}
+              value={password}
               required
-              onChange={this.handleInputChangeFor('password')}
+              onChange={(event) => this.handleInputChangeFor(event, 'password')}
             />
           </label>
+        </div>
+        <div>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Searchable</FormLabel>
+            <RadioGroup value={searchable} onChange={(event) => this.handleInputChangeFor(event, searchable)}>
+              <FormControlLabel value='true' control={<Radio />} label="Yes" />
+              <FormControlLabel value='false' control={<Radio />} label="No" />
+            </RadioGroup>
+          </FormControl>
         </div>
         <div>
           <input className="btn" type="submit" name="submit" value="Register" />
