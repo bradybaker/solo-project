aab505fd9cba91e9cb7c18c650fff082f53e7c68
Brady Baker <bradybaker82@gmail.com>
2021-01-18 09:32:08 -0600

    Specific followed user brands are being sent to client side, next step is to append

--- src/components/FollowPage/FollowedUserCard.js
+++ src/components/FollowPage/FollowedUserCard.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router'
 import '../App/App.css'
 
 
 const FollowedUserCard = (props) => {
+    const history = useHistory();
+    const { id, f_name, l_name } = props.user
 
-    const { f_name, l_name } = props.user
+    const goToCloset = (id, fname, lname) => {
+        console.log('Going to this brands closet!', id)
+
+        history.push({
+            pathname: `/followedUserCloset`,
+            search: `?userid=${id}&fname=${fname}$lname=${lname}`,
+        });
+    }
 
     return (
         <>
-            <div className='card hvr-float'>
+            <div className='card hvr-float' onClick={() => goToCloset(id, f_name, l_name)}>
                 <h2>{f_name} {l_name}</h2>
             </div>
         </>
--- src/components/FollowedUserCloset/FollowedUserCloset.jsx
+++ src/components/FollowedUserCloset/FollowedUserCloset.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 
 const FollowedUserCloset = () => {
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const params = new URLSearchParams(location.search.substring(1))
+    const userID = params.get('userid')
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_USER_BRAND', payload: userID })
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <div >
             <h1>Followed user closet</h1>
