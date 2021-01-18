1a43e2b299b0eab24added1177ed42ae9815ed6a
Brady Baker <bradybaker82@gmail.com>
2021-01-18 14:42:33 -0600

    Followed user specifc brand clothing items are appending on the DOM

--- src/components/FollowedUserCloset/FollowedUserBrandCloset.jsx
+++ src/components/FollowedUserCloset/FollowedUserBrandCloset.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import '../App/App.css'
 
-const followedUserBrandCloset = () => {
+
+function FollowedUserBrandCloset() {
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const followedUserClothing = useSelector(store => store.userClothingReducer)
+    const params = new URLSearchParams(location.search.substring(1))
+    const uID = params.get('userid')
+    const fName = params.get('fname');
+    const lName = params.get('lname')
+    const brandName = params.get('brandname')
+    const bID = params.get('brandid')
+
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_USER_CLOTHING', payload: { brandID: bID, userID: uID } })
+        // eslint-disable-next-line
+    }, []);
+
+
     return (
         <div >
-            Followed User Specific Brand closet
+            <h1>{fName} {lName}'s {brandName} Closet</h1>
+            <div className='brandCardContainer'>
+                {
+                    followedUserClothing.map(item => {
+                        return (
+                            <div className='card hvr-float' key={item.id}>
+                                <h2>{item.item_name}</h2>
+                                <h2>{item.item_size}</h2>
+                                <h2>{item.item_note}</h2>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
 
-export default followedUserBrandCloset
+export default FollowedUserBrandCloset;
--- src/redux/sagas/clothingSaga.js
+++ src/redux/sagas/clothingSaga.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function* fetchUserClothing(action) {
     try {
-        const response = yield axios.get(`/api/closet/${action.payload}`)
+        const response = yield axios.get(`/api/closet/${action.payload.userID}/${action.payload.brandID}`)
         yield put({ type: 'SET_USER_CLOTHING', payload: response.data })
     } catch (error) {
         console.log('Error in fetchUserClothing Saga', error)
