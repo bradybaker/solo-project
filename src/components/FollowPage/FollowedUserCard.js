8eeb739b6912e190e5caa447aed62c536c017e8a
Brady Baker <bradybaker82@gmail.com>
2021-01-21 09:26:38 -0600

    Unfollow buttons are conditionally rendering and unfollow function is made

--- src/components/FollowPage/FollowedUserCard.js
+++ src/components/FollowPage/FollowedUserCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useHistory } from 'react-router'
+import { useDispatch, useSelector } from 'react-redux'
 import '../App/App.css'
 
 
 const FollowedUserCard = (props) => {
     const history = useHistory();
+    const dispatch = useDispatch();
+    const userInfo = useSelector(store => store.user)
     const { id, f_name, l_name } = props.user
+
 
     const goToCloset = (id, fname, lname) => {
         console.log('Going to this brands closet!', id)
@@ -16,10 +20,19 @@ const FollowedUserCard = (props) => {
         });
     }
 
+    const unfollowUser = (id) => {
+        dispatch({ type: 'UNFOLLOW_USER', payload: { friendID: id, userID: userInfo.id } })
+    }
+
     return (
         <>
             <div className='card hvr-float' onClick={() => goToCloset(id, f_name, l_name)}>
                 <h2>{f_name} {l_name}</h2>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+                {props.editMode &&
+                    <i class="fas fa-user-times deleteIcon" onClick={() => unfollowUser(id)}></i>
+                }
             </div>
         </>
     )
